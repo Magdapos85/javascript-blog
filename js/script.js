f1791f1cb9408c5618218425b3fe30f2d0289b5d
Magdalena Pospiech <madzia.p3@wp.pl>
2021-03-23 17:56:16 +0100

    new change

--- js/script.js
+++ js/script.js
@@ -4,8 +4,13 @@ document.getElementById('test-button').addEventListener('click', function(){
   console.log('links:', links);
 });
 const titleClickHandler = function(event){
+    event.preventDefault();
+  const clickedElement = this;
+  const clickedElement = active;
+
   console.log('Link was clicked!');
   console.log(event);
+
   /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -16,6 +21,7 @@ const titleClickHandler = function(event){
 
   /* add class 'active' to the clicked link */
 
+  console.log('clickedElement:', clickedElement);
   /* remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.articles a.active');
@@ -25,11 +31,19 @@ const titleClickHandler = function(event){
 }
 
   /* get 'href' attribute from the clicked link */
+   const articleSelector = href;
+  atrybut = element.articleSelector(href);
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector (href);
+  console.log (article);
+
   /* add class 'active' to the correct article */
 }
+const targetArticle = document.querySelector ('.articles a.active');
+console.log (article);
 
 const links = document.querySelectorAll('.titles a');
 
