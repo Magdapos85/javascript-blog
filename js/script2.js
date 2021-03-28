02bf49f812c8c04351c1b5a39d74ae0b8771a0a4
Magdalena Pospiech <madzia.p3@wp.pl>
2021-03-28 10:26:10 +0200

    new

--- js/script2.js
+++ js/script2.js
@@ -1,37 +1,47 @@
+'use strict';
 document.getElementById('test-button').addEventListener('click', function(){
   const links = document.querySelectorAll('.titles a');
   console.log('links:', links);
 });
 const titleClickHandler = function(event){
+    event.preventDefault();
   const clickedElement = this;
-  event.preventDefault();
-  console.log('Link was clicked!');
 
-  /* [DONE] remove class 'active' from all article links  */
+
+  console.log('Link was clicked!');
+  console.log(event);
+
+  /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-  }
+  activeLink.classList.remove('active');
+}
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
+  /* remove class 'active' from all articles */
 
-  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.articles a.active');
+
+  for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
+   const articleSelector = href;
+  atrybut = element.articleSelector(href);
+  console.log(articleSelector);
 
-  const articleSelector = href;
- atrybut = element.articleSelector(href);
- console.log(articleSelector);
- 
   /* find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector (href);
+  console.log (article);
 
   /* add class 'active' to the correct article */
 }
-
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
